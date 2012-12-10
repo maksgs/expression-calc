74f077f860de9508bdfcc0184a2b7fbf8d1653f8
Skif <skifpao@gmail.com>
2012-12-10 17:07:53 +0200

    update

--- src/main/java/math/controllers/UserController.java
+++ src/main/java/math/controllers/UserController.java
@@ -6,6 +6,7 @@ import math.hibernate.UserMethods;
 import math.model.Role;
 import math.model.User;
 import org.springframework.beans.PropertyValues;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
@@ -13,63 +14,49 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+
 @Controller
 public class UserController
 {
     private UserMethods um = new UserMethods();
     private RoleMethods rm = new RoleMethods();
 
-//    public UserController(){
-//        setCommandClass(User.class);
-//
-//        setCommandName("user");
-//    }
-
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String regInit(ModelAndView mav){
-//
-      return "registration";
+    public ModelAndView regInit(ModelAndView mav){
+    mav.addObject("user", new User());
+      return mav;
     }
 
-    @RequestMapping(value = "/newuser", method = RequestMethod.GET)
-    public String addUser(@RequestParam String name,
-                          @RequestParam String pwd1,
-                          @RequestParam String pwd2
-                        ){
-//        RegistrarionValidator rv = new RegistrarionValidator();
-//        User user = new User();
-//        DataBinder binder = new DataBinder(user);
-//        binder.setDisallowedFields(new String[]{"id", "role", "date", "enabled"});
-//        binder.setValidator(rv);
-//        binder.validate();
-//        BindingResult result =  binder.getBindingResult();
-//        binder.bind(PropertyValues);
-//       if(result.hasErrors()){
-//            return "registration";
-//        }else{
+    @RequestMapping(value = "/newuser", method = RequestMethod.POST)
+    public String addUser(  @Valid User user,
+                            BindingResult result){
+        if(result.hasErrors()){
+            return "registration";
+        }
+        else{
         User usr = new User();
-            usr.setName(name);
-            usr.setPassword(pwd1);
+            usr.setName(user.getName());
+            usr.setPassword(user.getPassword());
             usr.setEnabled(true);
             usr.setDate();
             usr.setRole(rm.getUserRole());
             um.saveUser(usr);
             return "redirect:/login.html";
-    //}
-
+        }
     }
 
-    /*@InitBinder
-    public void initBinder(WebDataBinder dataBinder) {
 
-        dataBinder.setDisallowedFields(new String[] {"id"});
 
-        dataBinder.setRequiredFields(new String[] {"name", "area", "population", "currency"});
-
-        dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(false));
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
-        dateFormat.setLenient(false);
-        dataBinder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
-    }       */
+//    @InitBinder
+//    public void initBinder(WebDataBinder dataBinder) {
+//
+//        dataBinder.setDisallowedFields(new String[] {"id","date","enabled","role"});
+//
+//        dataBinder.setRequiredFields(new String[] {"name", "password", "confirmPassword"});
+//
+//        dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(false));
+//
+//        ;
+//    }
 }
--- src/main/webapp/jsp/registration.jsp
+++ src/main/webapp/jsp/registration.jsp
@@ -11,34 +11,34 @@
 </head>
 <body>
 
-    <%--<form:form action="registration.html">--%>
-    <form action="/newuser.html" >
+    <form:form method="post" commandName="user" action="newuser.html">
+    <%--<form action="/newuser.html" >--%>
     <table>
         <tr>
             <td>Login</td>
-            <%--<td><form:input path="name" /></td>--%>
-                <td><input id="login" type="text" name="name" max_length="20" size="20" /> </td>
-            <%--<td><form:errors path="name" cssClass="error" /></td>--%>
+            <td><form:input path="name" /></td>
+                <%--<td><input id="login" type="text" name="name" max_length="20" size="20" /> </td>--%>
+            <td><form:errors path="name" cssClass="error" /></td>
         </tr>
         <tr>
             <td>Password:</td>
-            <td><input type='password'  name="pwd1" id="pwd1" value="<c:out value="${pwd1}"/>" /></td>
-            <%--<td><form:password path="password" /></td>--%>
-            <%--<td><form:errors path="password" cssClass="error" /></td>--%>
+            <%--<td><input type='password'  name="pwd1" id="pwd1" value="<c:out value="${pwd1}"/>" /></td>--%>
+            <td><form:password path="password" /></td>
+            <td><form:errors path="password" cssClass="error" /></td>
         </tr>
         <tr>
             <td>Confirm Password :</td>
-            <td><input type='password'  name="pwd2" id="pwd2" value="<c:out value="${pwd2}"/>" /></td>
-            <%--<td><form:password path="confirmPassword" /></td>--%>
-            <%--<td><form:errors path="confirmPassword" cssClass="error" /></td>--%>
+            <%--<td><input type='password'  name="pwd2" id="pwd2" value="<c:out value="${pwd2}"/>" /></td>--%>
+            <td><form:password path="confirmPassword" /></td>
+            <td><form:errors path="confirmPassword" cssClass="error" /></td>
         </tr>
         <tr>
-            <td><button onclick="registerUser();" >Register</button></td>
-            <%--<td><input type="submit" value="REGISTER"></td>--%>
-            <td><button id="back" onclick="window.history.back()">Back</button> </td>
+            <%--<td><button onclick="registerUser();" >Register</button></td>--%>
+            <td><input type="submit" value="REGISTER"></td>
+            <%--<td><button id="back" onclick="window.history.back()">Back</button> </td>--%>
         </tr>
     </table>
-    </form>
-    <%--</form:form>--%>
+    <%--</form>--%>
+    </form:form>
                                                                                 </body>
 </html>
